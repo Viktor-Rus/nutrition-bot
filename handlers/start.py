ea3d5c67e62c116de88575b81f191f6a6d26f11a
virus <crimeacriminal@gmail.com>
2026-07-07 12:46:23 +0300

    edit

--- handlers/start.py
+++ handlers/start.py
@@ -3,7 +3,9 @@ from aiogram.filters import Command
 
 from keyboards import MENU_HELP, help_actions_keyboard, help_text
 from services.payments import (
+    activate_free_trial,
     get_subscription,
+    has_used_trial,
     start_offer_keyboard,
     start_offer_text,
 )
@@ -16,6 +18,10 @@ def register(dp: Dispatcher):
     async def start(message: types.Message):
         maybe_upsert_private_user(message)
         subscription = get_subscription(message.from_user.id)
+
+        if not has_used_trial(subscription):
+            activate_free_trial(message.from_user.id)
+            subscription = get_subscription(message.from_user.id)
 
         await message.answer(
             start_offer_text(subscription),
